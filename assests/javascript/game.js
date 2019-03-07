6d84e5cb9af1dc8b1e2ab353f08df9b5fc13e92e
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-03-07 12:16:04 -0500

    added variables for game

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,9 +1,22 @@
-<script type="text/javascript">
+// Variable declaring letters that can be used
+var allLetters = "abcdefghijklmnopqrstuvwxyz"
+// Variable declaring wins
+var wins = 0;
+// Variable declaring losses
+var loss = 0;
+// Variable declaring guesses a user has
+var howManyGuesses = 10;
+// Variable with array declaring letters already used
+var usedLetters = []
 
-{/* create prompt to alert user of the game rules */
+
+function rules() {
+    alert("Click the button for your random number selection")
+}
+function rules ();
 
 
 
-
-
-</script>
+// allows user to click button for their random letter
+document.getElementById{"button"}.onclick = function()
+    
