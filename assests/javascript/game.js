00b0358eca3f7e51613ff5529693bea19d61b87c
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-03-09 20:21:31 -0500

    Game functions work, need to be able to restart after a win or loss or zero tries remaining

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -14,10 +14,12 @@ var usedLetters = [];
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var tiesText = document.getElementById("ties-text");
+
+var guessedText = document.getElementById("guessed-text");
+// To not disply the computers choice
+// var computerChoiceText = document.getElementById("computerchoice-text");
 
 // Generate computer random answer. Set a random letter to return as the computer guess. Console log the computer guess.
 var computerGuess = letter[Math.floor(Math.random () * letter.length)]
@@ -32,26 +34,41 @@ usedLetters.push(userChoice)
 
 // If statement when user guesses computer random letter, Wins!!
 if (userChoice === computerGuess){
-    // Increases the wins by 1
+// Increases the wins by 1
      wins++;
-    //  Resets the guesses to 10
+//  Resets the guesses to 10
     remainingGuesses = 10;
-    // Resets the letters previously guessed to 0
-    remainingGuesses = 0;
-    // Resets the cuputer random letter
-    computerGuess = letter[Math.floor(Math.random () * letter.length)]
+// Resets the letters previously guessed to 0
+    usedLetters = 0;
+// Resets the cuputer random letter
+    computerGuess = letter[Math.floor(Math.random () * letter.length)];
+}
  
-
-
+// If user runs out of guesses
+else if (remainingGuesses === 0){
+// Loses increase after all guesses have been used
+losses++;
+//  Resets the guesses to 10
+remainingGuesses = 10;
+ // Resets the letters previously guessed to 0
+ usedLetters = 0;
+ // Resets the cuputer random letter
+ computerGuess = letter[Math.floor(Math.random () * letter.length)]
 }
 
- // Display the user and computer guesses, and wins/losses/ties.
- userChoiceText.textContent = "You chose: " + userChoice;
- 
-//  User can not view what letter computer chose
-//  computerChoiceText.textContent = "The computer chose: " + computerGuess;
+// Decrease numbr of remaining guesses if user did not win or reach 0 remaining guesses
+else {
+    remainingGuesses--;
+}
+
+ // Display the user and computer guesses, and wins/losses.
+ userChoiceText.textContent = "Guessed Letters: " + usedLetters;
  winsText.textContent = "Wins: " + wins;
  lossesText.textContent = "Losses: " + losses;
- tiesText.textContent = "Ties: " + ties;
+ guessedText.textContent = "Remaining Tries: " + remainingGuesses;
+//  User can not view what letter computer chose
+//  computerChoiceText.textContent = "The computer chose: " + computerGuess;
 }
+ 
+
 
